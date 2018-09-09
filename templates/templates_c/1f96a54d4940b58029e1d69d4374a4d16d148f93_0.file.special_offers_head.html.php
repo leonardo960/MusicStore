6bdf74a56a70c24b103f36817ed3dabe4d15325d
Samuele Petrucci <theworstcommentever@gmail.com>
2018-09-09 10:37:52 +0200

    Recent-albums html/css added + small fixes to other files

--- templates/templates_c/1f96a54d4940b58029e1d69d4374a4d16d148f93_0.file.special_offers_head.html.php
+++ templates/templates_c/1f96a54d4940b58029e1d69d4374a4d16d148f93_0.file.special_offers_head.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.32, created on 2018-09-08 13:58:52
+/* Smarty version 3.1.32, created on 2018-09-09 07:54:24
   from 'C:\wamp64\www\MusicStore\MusicStore\templates\special_offers_head.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.32',
-  'unifunc' => 'content_5b93d59c9cfe97_38851454',
+  'unifunc' => 'content_5b94d1b05ae694_54822348',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '1f96a54d4940b58029e1d69d4374a4d16d148f93' => 
     array (
       0 => 'C:\\wamp64\\www\\MusicStore\\MusicStore\\templates\\special_offers_head.html',
-      1 => 1536414185,
+      1 => 1536479634,
       2 => 'file',
     ),
   ),
@@ -20,10 +20,10 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5b93d59c9cfe97_38851454 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5b94d1b05ae694_54822348 (Smarty_Internal_Template $_smarty_tpl) {
 ?><head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>Musicstore a Entertainment Category Flat Responsive web template | Home :: W3layouts</title>
+		<title>Music Store - Special Offers</title>
 		<link href="../css/style.css" rel="stylesheet" type="text/css"  media="all" />
 		<link href='//fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="../css/responsiveslides.css">
