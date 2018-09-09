82b7d5ff0015452237fb73b1cefd72d349607ec9
Samuele Petrucci <theworstcommentever@gmail.com>
2018-09-09 09:35:54 +0200

    Solving conflicts

--- templates/templates_c/454082ea0f89882653f558a0c3f0abd0a6fc92b2_0.file.not_logged_in_header.html.php
+++ templates/templates_c/454082ea0f89882653f558a0c3f0abd0a6fc92b2_0.file.not_logged_in_header.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.32, created on 2018-09-07 10:38:23
+/* Smarty version 3.1.32, created on 2018-09-09 07:33:29
   from 'C:\wamp64\www\MusicStore\MusicStore\templates\not_logged_in_header.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.32',
-  'unifunc' => 'content_5b92551f77d302_28590774',
+  'unifunc' => 'content_5b94ccc9cf2a53_72936103',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '454082ea0f89882653f558a0c3f0abd0a6fc92b2' => 
     array (
       0 => 'C:\\wamp64\\www\\MusicStore\\MusicStore\\templates\\not_logged_in_header.html',
-      1 => 1534150888,
+      1 => 1536415821,
       2 => 'file',
     ),
   ),
@@ -20,6 +20,6 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5b92551f77d302_28590774 (Smarty_Internal_Template $_smarty_tpl) {
-?><li><a href="login.php">log in</li></a><?php }
+function content_5b94ccc9cf2a53_72936103 (Smarty_Internal_Template $_smarty_tpl) {
+?><li><a href="login.php">Log In</a></li><?php }
 }
