88995eeff22c643fea3ced392d2522daf5ec8a5b
Samuele Petrucci <theworstcommentever@gmail.com>
2018-09-12 21:10:10 +0200

    New layout + new assets added

--- templates/templates_c/5086596fa0a79de9127225ea388477cd5cd0aed3_0.file.frame_public.html.php
+++ templates/templates_c/5086596fa0a79de9127225ea388477cd5cd0aed3_0.file.frame_public.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.32, created on 2018-09-09 15:19:55
+/* Smarty version 3.1.32, created on 2018-09-12 17:57:43
   from 'C:\wamp64\www\MusicStore\MusicStore\templates\frame_public.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.32',
-  'unifunc' => 'content_5b953a1b801f56_05579075',
+  'unifunc' => 'content_5b995397967104_14499492',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '5086596fa0a79de9127225ea388477cd5cd0aed3' => 
     array (
       0 => 'C:\\wamp64\\www\\MusicStore\\MusicStore\\templates\\frame_public.html',
-      1 => 1536506393,
+      1 => 1536775061,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5b953a1b801f56_05579075 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5b995397967104_14499492 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE HTML>
 <html>
 <?php $_smarty_tpl->_subTemplateRender(((string)$_smarty_tpl->tpl_vars['head']->value), $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, true);
@@ -33,9 +33,18 @@ function content_5b953a1b801f56_05579075 (Smarty_Internal_Template $_smarty_tpl)
 			<!---start-top-header--->
 			<div class="top-header">
 				<div class="top-header-left">
+				<div class="logo">
+					<a href="index.php"><img src="../images/logo3.png" title="Music Store" /></a>
+				</div>
 				</div>
 				<div class="top-header-right">
 					<ul>
+						<li>
+							<form class="search" action="search.php" type="GET">
+								<input type="text" name="query" pattern="[A-Za-z0-9\s]+" title="Please use only upper-case/lower-case letters, digits and blank spaces. Thank you."/>
+								<button type="submit"  value=""><img src="../images/search_icon_small.png"></button>	
+							</form>
+						</li>
 						<?php if (isset($_smarty_tpl->tpl_vars['logged_username']->value)) {?>
 						<li>Logged in as: <a id="header-auth-info"><?php echo $_smarty_tpl->tpl_vars['logged_username']->value;?>
 </a></li>
@@ -52,21 +61,7 @@ function content_5b953a1b801f56_05579075 (Smarty_Internal_Template $_smarty_tpl)
 			</div>
 			<!--end-top-header-->
 			<div class="clear"> </div>
-			<!--start-sub-header-->
-			<div class="sub-header">
-				<div class="logo">
-					<a href="index.php"><img src="../images/logo2.png" title="Music Store" /></a>
-				</div>
-				<div class="sub-header-center">
-					<form class="search" action="search.php" type="GET">
-						<input type="text" name="query" pattern="[A-Za-z0-9\s]+" title="Please use only upper-case/lower-case letters, digits and blank spaces. Thank you.""><input type="submit"  value="search" />
-					</form>
-				</div>
-				<div class="sub-header-right">
-				</div>
-				<div class="clear"> </div>
-			</div>
-			<!--end-sub-header-->
+			
 			<div class="clear"> </div>
 			<!--start-top-nav-->
 			<div class="top-nav">
@@ -98,7 +93,10 @@ function content_5b953a1b801f56_05579075 (Smarty_Internal_Template $_smarty_tpl)
 						<li><a href="#">Legal Notice</a></li>
 					</ul>
 				</div>
-				<div class="footer-divider"></div>
+				<div class="col_1_of_4 span_1_of_4">
+					<h3>Payment</h3>
+					<a href=""><img id="methods-payment-img" src="../images/methods_payment_icons.png"></a>
+				</div>
 				<div class="col_1_of_4 span_1_of_4 footer-lastgrid">
 					<h3>Get in touch</h3>
 					<ul>
@@ -107,8 +105,6 @@ function content_5b953a1b801f56_05579075 (Smarty_Internal_Template $_smarty_tpl)
 						<li><a href="#"><img src="../images/rss.png" title="Rss" /></a></li>
 						<li><a href="#"><img src="../images/gpluse.png" title="Google+" /></a></li>
 					</ul>
-					<h3>Methods of Payment</h3>
-					<a href=""><img id="methods-payment-img" src="../images/methods_payment_icons.png"></a>
 				</div>
 			</div>
 			</div>
--- templates/templates_c/bbe7a471f77f116396ae75f9e182c18e4158f02c_0.file.index_content.html.php
+++ templates/templates_c/bbe7a471f77f116396ae75f9e182c18e4158f02c_0.file.index_content.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.32, created on 2018-09-11 14:55:37
+/* Smarty version 3.1.32, created on 2018-09-12 18:24:38
   from 'C:\wamp64\www\MusicStore\MusicStore\templates\index_content.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.32',
-  'unifunc' => 'content_5b97d769f22bf8_26430804',
+  'unifunc' => 'content_5b9959e63eebc0_86024167',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'bbe7a471f77f116396ae75f9e182c18e4158f02c' => 
     array (
       0 => 'C:\\wamp64\\www\\MusicStore\\MusicStore\\templates\\index_content.html',
-      1 => 1536677497,
+      1 => 1536776676,
       2 => 'file',
     ),
   ),
@@ -20,14 +20,15 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5b97d769f22bf8_26430804 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5b9959e63eebc0_86024167 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!--start-image-slider---->
 					<div class="image-slider">
 						<!-- Slideshow 1 -->
 					    <ul class="rslides" id="slider1">
-					      <li><img src="../images/slider1.jpg" alt=""></li>
-					      <li><img src="../images/slider3.jpg" alt=""></li>
-					      <li><img src="../images/slider1.jpg" alt=""></li>
+					      <li><img src="../images/slider5.png" alt=""></li>
+					      <li><img src="../images/slider6.png" alt=""></li>
+					      <li><img src="../images/slider7.png" alt=""></li>
+						  <li><img src="../images/slider4.png" alt=""></li>
 					    </ul>
 						 <!-- Slideshow 2 -->
 					</div>
@@ -43,12 +44,10 @@ if ($_from !== null) {
 foreach ($_from as $_smarty_tpl->tpl_vars['album']->value) {
 ?>
 							<div class="grid_1_of_5 images_1_of_5 displayed-item">
+								<h3>"<?php echo $_smarty_tpl->tpl_vars['album']->value['nome_album'];?>
+"</h3>
 								<img src="<?php echo $_smarty_tpl->tpl_vars['album']->value['img_path'];?>
 ">
-								<h3><?php echo $_smarty_tpl->tpl_vars['album']->value['nome_album'];?>
-</h3>
-								<p><?php echo $_smarty_tpl->tpl_vars['album']->value['descrizione'];?>
-</p>
 								<h4><span></span><?php echo $_smarty_tpl->tpl_vars['album']->value['prezzo'];?>
 &euro;</h4>
 								<div class="button"><span><a href="albumpage.php?id_album=<?php echo $_smarty_tpl->tpl_vars['album']->value['id_album'];?>
@@ -76,12 +75,10 @@ if ($_from !== null) {
 foreach ($_from as $_smarty_tpl->tpl_vars['album']->value) {
 ?>
 							<div class="grid_1_of_5 images_1_of_5 displayed-item">
+								 <h3>"<?php echo $_smarty_tpl->tpl_vars['album']->value['nome_album'];?>
+"</h3>
 								 <img src="<?php echo $_smarty_tpl->tpl_vars['album']->value['img_path'];?>
 ">
-								 <h3><?php echo $_smarty_tpl->tpl_vars['album']->value['nome_album'];?>
-</h3>
-								 <p><?php echo $_smarty_tpl->tpl_vars['album']->value['descrizione'];?>
-</p>
 								 <h4><span><?php echo $_smarty_tpl->tpl_vars['album']->value['prezzo'];?>
 &euro;</span><?php echo $_smarty_tpl->tpl_vars['album']->value['prezzo_offerta'];?>
 &euro;</h4>
