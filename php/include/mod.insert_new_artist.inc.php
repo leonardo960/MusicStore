9ac1ee09f61f01c221a1cc5b27c37d20c325cca3
mattefic <matteo.ficorilli@libero.it>
2018-09-16 19:32:15 +0200

    Insert not working

--- php/include/mod.insert_new_artist.inc.php
+++ php/include/mod.insert_new_artist.inc.php
@@ -5,19 +5,26 @@
 	}
 	
 	if($_POST['fine_attivita'] === ''){
-		$ps = $db->get_connection()->prepare("insert into artisti (nome_artista, biografia, inizio_attivita, genere, album_pubblicati) values (?, ?, ?, ?, ?)");
-		$ps->bind_param('ssiii', $_POST["nome_artista"], $_POST["biografia"], $_POST["inizio_attivita"], $_POST["genere"], $_POST['album_pubblicati']);
+		$ps = $db->get_connection()->prepare("insert into artisti (nome_artista, biografia, inizio_attivita, album_pubblicati) values (?, ?, ?, ?)");
+		$ps->bind_param('ssiii', $_POST["nome_artista"], $_POST["biografia"], $_POST["inizio_attivita"], $_POST['album_pubblicati']);
 		$ps->execute();
 	} else {
-		$ps = $db->get_connection()->prepare("insert into artisti (nome_artista, biografia, inizio_attivita, fine_attivita, genere, album_pubblicati) values (?, ?, ?, ?, ?, ?)");
-		$ps->bind_param('ssiiii', $_POST["nome_artista"], $_POST["biografia"], $_POST["inizio_attivita"], $_POST["fine_attivita"], $_POST["genere"], $_POST['album_pubblicati']);
+		$ps = $db->get_connection()->prepare("insert into artisti (nome_artista, biografia, inizio_attivita, fine_attivita, album_pubblicati) values (?, ?, ?, ?, ?)");
+		$ps->bind_param('ssiiii', $_POST["nome_artista"], $_POST["biografia"], $_POST["inizio_attivita"], $_POST["fine_attivita"], $_POST['album_pubblicati']);
 		$ps->execute();
 	}
-																											 
+
+
 	
 	$result = $db->getResult("select id_artista from artisti where biografia = '{$_POST["biografia"]}'");
 	$ext = pathinfo($_FILES['artist-img']['name'], PATHINFO_EXTENSION);
 	$target_file = $target_dir . $result[0]['id_artista'] . '.' . $ext;
 	move_uploaded_file($_FILES["artist-img"]["tmp_name"], $target_file);
 	$db->query("update artisti set img_path = '{$target_file}' where id_artista = '{$result[0]["id_artista"]}'");
+    //GENERI
+    foreach ($_POST["generi[]"] as $value) {
+        $ps = $db->get_connection()->prepare("insert into genere_artisti (genere, artista) values (?, ?)");
+        $ps->bind_param('si',$value,$result[0]['id_artista']);
+        $ps->execute();
+    }
 ?>
